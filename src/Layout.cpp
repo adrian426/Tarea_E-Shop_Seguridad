bd1f5328a011de2bf6588085002cd233a65703c7
Adrian Alvarez <adrian426alv@gmail.com>
2019-10-15 00:24:09 -0600

    Remove http from h.refs

--- src/Layout.cpp
+++ src/Layout.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 void printOptions(bool sessionAlive){
-    cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/Home'>Home</a><br>";
-    cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/review_claims'>Feedback</a><br>";
-    //cout << "<input type='button' value='AddClaim' onclick=\"location.href='http://localhost/cgi-bin/Tarea1_Seguridad/add_claim'\"><br>";
+    cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/Home'>Home</a><br>";
+    cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/review_claims'>Feedback</a><br>";
+    //cout << "<input type='button' value='AddClaim' onclick=\"location.href='localhost/cgi-bin/Tarea1_Seguridad/add_claim'\"><br>";
     if(sessionAlive == false){
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/login'>Sign in</a><br>";
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/sign_up'>Sign Up</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/login'>Sign in</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/sign_up'>Sign Up</a><br>";
     } else {
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/add_product'>Sell a Product</a><br>";
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/review_cart'>Shopping Cart</a><br>";
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/history'>Shopping History</a><br>";
-        cout << "<a href='http://172.24.129.95/cgi-bin/Tarea1_Seguridad/logout'>Sign out</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/add_product'>Sell a Product</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/review_cart'>Shopping Cart</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/history'>Shopping History</a><br>";
+        cout << "<a href='172.24.129.95/cgi-bin/Tarea1_Seguridad/logout'>Sign out</a><br>";
     }
 }
